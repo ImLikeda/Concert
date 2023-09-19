d3734d16b40441d463a161a9f4c362fb2e805c21
ImLikeda <whwogus4304@naver.com>
2023-09-19 11:35:15 +0900

    last[-2] commit

--- static/js/teamtitle.js
+++ static/js/teamtitle.js
@@ -2,5 +2,7 @@ var teamtitle = {
     omniA: "1dXIzXA2C-B4qiRLLoUrdlDTYHWGH-hus",
     omniB: "12Ws-wrf0ESHXejd-8mv0jsFMY7qdRRu_",
     comaA: "1l-Df3xcUj3cITQumIVza-LoklUbntZZM",
-    comaB: "11fhItzZBCK9PjUy1fliN6mujfcfLSQu4"
+    comaB: "11fhItzZBCK9PjUy1fliN6mujfcfLSQu4",
+    collaboA: "1RbnWVOSI_mCTweb90NikG0XSXnFEj9gy",
+    collaboB: "1DWBadIQ-O9tCettjf4iL1xePoUTUGmM1"
 }
